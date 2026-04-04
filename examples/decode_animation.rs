e098bfc8c7e4b33795ec5fd9ab9e4f99a3340874
Mika Vatanen <blaind@blaind.net>
2026-04-04 20:40:01 +0300

    Fix test

--- examples/decode_animation.rs
+++ examples/decode_animation.rs
@@ -8,16 +8,15 @@ fn main() {
     let decoder = Decoder::new(&buffer).unwrap();
 
     for frame in decoder.into_iter() {
-        assert_eq!(frame.dimensions(), (400, 400));
-        assert_eq!(frame.data().len(), 400 * 400 * 4); // w * h * rgba
+        let dimensions = frame.dimensions();
+        let data_len = frame.data().len();
+
+        assert_eq!(dimensions, (400, 400));
+        assert_eq!(data_len, 400 * 400 * 4); // w * h * rgba
 
         #[cfg(feature = "image")]
         assert_eq!(frame.into_image().unwrap().dimensions(), (400, 400));
 
-        info!(
-            "Frame, dimensions={:?}, data_len={}",
-            frame.dimensions(),
-            frame.data().len()
-        );
+        info!("Frame, dimensions={:?}, data_len={}", dimensions, data_len);
     }
 }
